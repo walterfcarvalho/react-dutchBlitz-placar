b844b212e0658bb5d239e92db94ec61c702dc002
valter f carvalho <walterfcarvalho@gmail.com>
2022-06-02 15:00:16 +0100

    css by bootstrap 5

--- src/Components/DutchGame/Card/index.js
+++ src/Components/DutchGame/Card/index.js
@@ -3,31 +3,44 @@ import './card.css'
 
 const DutchCard = ({ logo, color, score, handleChange, isEndGame }) => {
 
+  function colapse(e){
+    console.log( 'ok')
+  }
+
   return (
-    <div className={`dutchCard  ${color}`}>
+    <div className={`dutchCard`}>
 
-      <img src={require(`./../../../images/${logo}.png`)} alt= "descricao" />
+        <div className="accordion-item" onBlur={colapse} >
+          <h2 className="accordion-header" id={`head${color}`}>
+            <button className={`accordion-button collapsed ${color}`} type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${color}`} aria-expanded="true" aria-controls={`collapse${color}`}>
 
-      <div className="scores">
-          <input min={0} max={99} className={color} id={`${color}-bp`} type="number"  value={score.bp} onChange={handleChange} disabled={isEndGame} />
-          <input min={0} max={99} className={color} id={`${color}-dp`} type="number" value={score.dp} onChange={handleChange}  disabled={isEndGame} />
-      </div>
+              <div className="imagem">
+                <img src={require(`./../../../images/${logo}.png`)} alt="descricao" />
+              </div>
 
-      <div className="scorehistory">
-        <span>{score.history}</span>
-      </div>
-
-      <div className="total">
-        <div className="totalScore">
-          {score.score}
+              <div className="scores">
+                <input min={0} max={99} className={`${color} bpdp`} id={`${color}-bp`} type="number" value={score.bp} onChange={handleChange} disabled={isEndGame} />
+                <input min={0} max={99} className={`${color} bpdp`} id={`${color}-dp`} type="number" value={score.dp} onChange={handleChange} disabled={isEndGame} />
+                <input min={0} max={99} className={`${color} bpdp`} id={`${color}-dp`} type="number" value={score.score}  />
+              </div>
+            </button>
+          </h2>
+          <div id={`collapse${color}`} className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+              <strong>History:</strong> {score.history}
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div>
-        {score.winner ? <img src={require(`./../../../images/crown.png`)} alt= "winner"/> : null }
-      </div>
+
+        <div>
+          {score.winner ? <img src={require(`./../../../images/crown.png`)} alt="winner" /> : null}
+        </div>
+
 
     </div>
+
+
   )
 }
 
--- src/Components/DutchGame/index.js
+++ src/Components/DutchGame/index.js
@@ -2,76 +2,98 @@ import React, { useState } from 'react'
 import DutchCard from './Card'
 import './DutchGame.css'
 
-
 const DutchGame = () => {
-  
+
   const itemPlacar = {
     bp: "",
     dp: "",
     score: 0,
     history: "",
-    winner:false
+    winner: false
   }
 
-  const [state, setState] = useState({ red:{...itemPlacar},green:{...itemPlacar},yellow:{...itemPlacar},blue:{...itemPlacar}, round: 1, endGame:false })
-  
+  const [state, setState] = useState({ red: { ...itemPlacar }, green: { ...itemPlacar }, yellow: { ...itemPlacar }, blue: { ...itemPlacar }, round: 1, endGame: false })
+
   const cores = ['green', 'red', 'yellow', 'blue']
 
   function handleScore(event) {
     event.preventDefault()
-    
+
     let valor = Object.assign({}, state)
+
+    let err = ""
+
+    cores.forEach( cor => (valor[cor].bp === "" || valor[cor].dp === "") ? err += `\r Pls fullfill ${cor} fields`: "")
+    if (err.length > 0){
+      window.alert(err)
+      return
+    }
 
     cores.forEach(cor => {
       valor[cor].score += valor[cor].bp - valor[cor].dp
-      valor[cor].history = valor[cor].history.concat( `(+${valor[cor].bp}-${valor[cor].dp})` ) 
-      valor[cor].bp = "" 
+      valor[cor].history = valor[cor].history.concat(`(+${valor[cor].bp}-${valor[cor].dp})`)
+      valor[cor].bp = ""
       valor[cor].dp = ""
-      
+
       if (valor[cor].score >= 75) {
         valor.endGame = true
         valor[cor].winner = true
       }
     })
-    
-    valor.round ++
 
+    valor.round++
 
     setState({ ...valor })
   }
 
   function handleChange(e) {
-      let corCampo = e.target.id.split('-')
+    let corCampo = e.target.id.split('-')
 
     let valor = state[corCampo[0]]
 
-    valor[corCampo[1]] = e.target.value   
+    valor[corCampo[1]] = e.target.value
 
-    setState({...state, [corCampo[0]]: valor })
+    setState({ ...state, [corCampo[0]]: valor })
 
   }
 
-  function newGame(e){
+  function newGame(e) {
     e.preventDefault()
 
-    setState({ red:{...itemPlacar},green:{...itemPlacar},yellow:{...itemPlacar},blue:{...itemPlacar}, round: 1, endGame:false })
+    setState({ red: { ...itemPlacar }, green: { ...itemPlacar }, yellow: { ...itemPlacar }, blue: { ...itemPlacar }, round: 1, endGame: false })
     console.log(state)
   }
 
   return (
     <div className="DuchContainer">
 
+      <div className='topo'>
+        <h3>Dutch Blitz</h3>
+        <input className="btn btn-light btn-sm button-topo" type="submit" value="New" onClick={newGame} />
+      </div>
+
+
       <div className='inicio'>
         <span>Round : {state.round}</span>
-        <input type="submit" value="New game" onClick={newGame} />
       </div>
 
       <form onSubmit={handleScore} id="form">
-        <input type="submit" value="Count"/>
-        <DutchCard logo="dutchCardYellow" color="yellow" score={state['yellow']} handleChange = {handleChange} isEndGame={state.endGame} />
-        <DutchCard logo="dutchCardRed" color="red" score={state['red']} handleChange = {handleChange} isEndGame={state.endGame} />
-        <DutchCard logo="dutchCardGreen" color="green" score={state['green']} handleChange = {handleChange} isEndGame={state.endGame} />
-        <DutchCard logo="dutchCardBlue" color="blue" score={state['blue']} handleChange = {handleChange} isEndGame={state.endGame} />
+        <div className="accordion" id={"head"}>
+
+          <DutchCard logo="dutchCardYellow" color="yellow" score={state['yellow']} handleChange={handleChange} isEndGame={state.endGame} />
+          <DutchCard logo="dutchCardRed" color="red" score={state['red']} handleChange={handleChange} isEndGame={state.endGame} />
+          <DutchCard logo="dutchCardGreen" color="green" score={state['green']} handleChange={handleChange} isEndGame={state.endGame} />
+          <DutchCard logo="dutchCardBlue" color="blue" score={state['blue']} handleChange={handleChange} isEndGame={state.endGame} />
+          
+          <div className="topo baixo">
+            <input type="submit" value="Count" />
+          </div>
+        </div>
+
+        <a href= "https://www.wikihow.com/Play-Dutch-Blitz" target="_blank">
+          Wikihow Dutch Blitz
+        </a>
+
       </form>
     </div>
   )
