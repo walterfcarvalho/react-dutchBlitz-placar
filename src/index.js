a6236b043627bdc720d2b502d68fbee51418ceb9
valter f carvalho <walterfcarvalho@gmail.com>
2022-06-12 19:54:50 +0100

    ajuste no build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DutchGame from './components/DutchGame.js'
+import DutchGame from './components/DutchGame'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
