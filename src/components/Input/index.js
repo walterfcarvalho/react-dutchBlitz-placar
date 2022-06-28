c63953d84e3c4f616cf0400830422d8131c8ce40
Kely Rufino <kelyrufino@Kelys-MacBook-Air.local>
2022-06-28 19:24:19 +0100

    Add CSS

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,15 +1,29 @@
-import React from 'react'
-import './input.css'  
+import React from "react";
+import "./input.css";
 
-const Input = ({color, score, handleChange, isDisabled, inputLabel, tpOp  }) => {
-
+const Input = ({
+  color,
+  score,
+  handleChange,
+  isDisabled,
+  inputLabel,
+  tpOp,
+}) => {
   return (
-    <div className='inputValor'>
+    <div className="inputValor">
       <strong>{inputLabel}</strong>
-      <input min={0} max={99} className={`${color} bpdp`} id={`${color}-${tpOp}`} type="number" value={score ? score: ""} onChange={handleChange} readOnly={isDisabled} />
+      <input
+        min={0}
+        max={99}
+        className="bpdp"
+        id={`${color}-${tpOp}`}
+        type="number"
+        value={score ? score : ""}
+        onChange={handleChange}
+        readOnly={isDisabled}
+      />
     </div>
-  )
+  );
+};
 
-}
-
-export default Input
+export default Input;
